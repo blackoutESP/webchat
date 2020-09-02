def751d4090690f69a49268836d19375e18e3b61
blackoutESP <yxion10101@gmail.com>
2020-09-02 13:01:22 +0200

    .

---
+++ front/src/app/webchat/webchat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-webchat',
+  templateUrl: './webchat.component.html',
+  styleUrls: ['./webchat.component.css']
+})
+export class WebchatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
