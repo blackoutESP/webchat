7758caa5a4505ecf9fe8cfb37f70cbf17ca52b6f
blackoutESP <yxion10101@gmail.com>
2020-09-15 09:04:24 +0200

    .

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatListModule } from '@angular/material/list';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { MatInputModule } from '@angular/material/input';
     MatTabsModule,
     MatListModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    ScrollingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
